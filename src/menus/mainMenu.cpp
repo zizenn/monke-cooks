f26d8a40698767d26866113d9657ea1f75263df8
sakif <zizenn@proton.me>
2026-07-19 17:13:38 +0600

    removed raygui cuz ill be using my own custom one

--- src/menus/mainMenu.cpp
+++ src/menus/mainMenu.cpp
@@ -1,11 +1,6 @@
-#include "raylib.h"
-#define RAYGUI_IMPLEMENTATION
-extern "C" {
-    #include "raygui.h"
-}
-
 #include "Color.hpp"
 #include "Rectangle.hpp"
+#include "raylib.h"
 #include "scenes.hpp"
 #include "time.hpp"
 #include <iostream>
@@ -13,28 +8,23 @@ extern "C" {
 static float posX = 0.0f;
 
 namespace {
-      Rectangle rects[1];
+
+Rectangle rects[1];
+
 }
 
-void scene::LoadMainMenu() {
-      rects[0] = {0, 0, 100, 50};
-}
+void scene::LoadMainMenu() { rects[0] = {0, 0, 100, 50}; }
 
 void scene::UpdateMainMenu() {
       posX += 100.0f * Time::DeltaTime();
       if (posX > 1280) {
             posX = 0.0f;
       }
-
 }
 
 void scene::DrawMainMenu() {
       raylib::Rectangle testRec = {posX, 20, 100, 100};
       testRec.Draw(raylib::Color::SkyBlue());
-      int result = GuiButton(rects[0], "my button");
-      if (result == 1) {
-            std::cout << "\nDIHHH\n";
-      }
 }
 
 void scene::UnloadMainMenu() {

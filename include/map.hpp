4ef50f07dc4ccbfcbb7dd02e0b2f8b2b7866e329
zizenn-pc <zizenn.69@gmail.com>
2026-08-01 17:17:12 +0930

    adding more things to the map

--- include/map.hpp
+++ include/map.hpp
@@ -2,13 +2,34 @@
 
 #include <sys/types.h>
 
+#include <string>
+
+#include "raylib.h"
+
 namespace map {
 
 // constants
-static const int TILE_SIZE = 32;
+static const int TILE_SIZE =
+      32;  // 32 means 32x32 image sizes for the texture, if the texture is
+           // greater, then this number it will be scaled to fit
 
-class Map {
-private:
+// enums
+enum class TileGroups {
+      Floor,
+      Counter,
 };
 
-} // namespace map
+// tile class
+class Tile {
+private:
+      TileGroups tileGroup_;
+};
+
+// map class
+class Map {
+private:
+      std::string mapFilePath_;
+      const Vector2 mapSize_ = {16, 9};
+};
+
+}  // namespace map

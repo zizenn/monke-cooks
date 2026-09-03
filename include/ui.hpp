5b74e5009331051657dde6dcc543e04e9bc49c1b
Hasan <sakif.hasan582@schools.sa.edu.au>
2026-09-03 21:22:17 +0930

    continuing on this

--- include/ui.hpp
+++ include/ui.hpp
@@ -1,21 +1,44 @@
 # pragma once
 
-#include "external/Vector2.hpp"
+#include <functional>
+#include "external/Rectangle.hpp"
 
 namespace ui {
+
+struct ButtonTexturePacket {
+      int baseTextureId;
+      int hoveredTextureId;
+      int clickedTextureId;
+};
 
 class Button {
 private:
       // visuals
-      raylib::Vector2 pos;
-      int width;
-      int height;
-      int textureId;
+      raylib::Rectangle buttonRect;
+      ButtonTexturePacket textures;
 
       // logic
       bool isClicked = false;
       bool isHovered = false;
+      std::function<void()> onClick;
 public:
+      Button(float x, float y, float width, float height, int basetextureId, int hoveredtextureId, int clickedtextureId, std::function<void()> function) {
+            buttonRect.x = x;
+            buttonRect.y = y;
+            buttonRect.width = width;
+            buttonRect.height = height;
+            textures.baseTextureId= basetextureId;
+            textures.hoveredTextureId = hoveredtextureId;
+            textures.clickedTextureId = clickedtextureId;
+            onClick = function;
+      };
+
+      Button(raylib::Rectangle rect, ButtonTexturePacket texturePacket, std::function<void()> function) {
+            buttonRect = rect;
+            textures = texturePacket;
+            onClick = function;
+      }
+
       void Draw();
       bool DetectUpdate();
 };
--- src/managers/ui.cpp
+++ src/managers/ui.cpp
@@ -1,11 +1,23 @@
 #include "ui.hpp"
+#include "Vector2.hpp"
 
 namespace ui {
       void Button::Draw() {
-            // add the draw function which draws a texture
+            // use rectangle draw function with the texture overload
       }
 
       bool Button::DetectUpdate() {
+            raylib::Vector2 mousePos = GetMousePosition();
+            bool tempClick = IsMouseButtonPressed(MOUSE_BUTTON_LEFT);
+            if (CheckCollisionPointRec(mousePos, buttonRect)) {
+                  isHovered = true;
+            } else if (CheckCollisionPointRec(mousePos, buttonRect) && tempClick == true) {
+                  isClicked = true;
+                  isHovered = true;
+            } else {
+                  isHovered = false;
+                  isClicked = false;
+            }
             return false;
       }
 };
